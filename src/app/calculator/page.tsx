e4449fdef2b07136c181ff366176fde716c1d94d
Naveen Cooray <naveen.cooray.nc@gmail.com>
2024-12-05 13:53:54 +0530

    Fix lint staged script

--- src/app/calculator/page.tsx
+++ src/app/calculator/page.tsx
@@ -3,13 +3,12 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Calculator - NextJS Starter Kit",
   description: "Calculate anything",
-  };
+};
 
 export default function Calculator() {
-    return (
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        Calculator
-      </div>
-    );
-  }
-  
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      Calculator
+    </div>
+  );
+}
--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,15 +1,14 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-title: "FAQ - NextJS Starter Kit",
-description: "Find answers to common questions about the calculator",
+  title: "FAQ - NextJS Starter Kit",
+  description: "Find answers to common questions about the calculator",
 };
 
 export default function Faq() {
-    return (
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        FAQ
-      </div>
-    );
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      FAQ
+    </div>
+  );
 }
-  
